0a7f020c2cd45c780947f657362740954bd8f87a
igee-17 <igbagboleye@gmail.com>
2024-02-21 10:34:59 +0100

    ambassadors page done

--- src/components/pages/Ambassadors.js
+++ src/components/pages/Ambassadors.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
+import SectionOne from '../ambassadors/SectionOne';
+import SectionTwo from '../ambassadors/SectionTwo';
+import SectionFour from '../ambassadors/SectionFour';
+import SectionThree from '../ambassadors/SectionThree';
 
 function Ambassadors() {
   return (
     <div>
       <Header />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
       <Footer />
-      
+
     </div>
   )
 }
--- src/components/sundays/TFC-sundays.js
+++ src/components/sundays/TFC-sundays.js
@@ -1,46 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, } from 'react-bootstrap';
 
+const backgroundImageUrl = 'background-img2.jpeg'; // Replace with your image URL
+
 const TFCsundays = () => {
-
-  const backgroundImageUrl = 'background-img2.jpeg'; // Replace with your image URL
-
-  const containerStyle = {
-    position: 'relative',
-    backgroundImage: `url(${backgroundImageUrl})`,
-    backgroundSize: 'cover', // Adjust as needed
-    backgroundPosition: 'center center', // Adjust as needed
-    backgroundRepeat: 'no-repeat', // Adjust as needed
-    width: '100%', // Set the width to 100% of the viewport width
-    height: '100vh', // Set the desired height
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'white', // Set text color
-    margin: 0, // Reset margin
-    padding: 0, // Reset padding
-    overflow: 'hidden',
-    boxSizing:'border-box',
-    marginBottom: '40px',
-  };
-
-  const overlayStyle = {
-    position: 'absolute',
-    top: 0,
-    left: '200px',
-    width: '60%', // Adjust to position the overlay to the left
-    height: '100%',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Adjust overlay color and opacity
-    zIndex: 1,
-  };
-
-  const textStyle = {
-    position: 'relative', // Ensure text is above the overlay
-    zIndex: 2, // Higher z-index than overlay
-    textAlign: 'left',
-    marginLeft: '20px',
-  };
 
 
   return (
@@ -48,19 +11,56 @@ const TFCsundays = () => {
 
       {/* Section 1 */}
       <section className="your-section-class">
-      <Container fluid style={containerStyle}>
-        {/* Overlay */}
-        <div style={overlayStyle}></div>
-        {/* Text */}
-        <div style={textStyle}>
-          <h2 style={{ fontSize: '110px', fontWeight: 'bolder', fontFamily: 'Sans-serif', color: '#000' }}>
-            Sundays @ <br/> TFC
-          </h2>
-        </div>
-      </Container>
+        <Container fluid style={containerStyle}>
+          {/* Overlay */}
+          <div style={overlayStyle}></div>
+          {/* Text */}
+          <div style={textStyle}>
+            <h2 style={{ fontSize: '110px', fontWeight: 'bolder', fontFamily: 'Sans-serif', color: '#000' }}>
+              Sundays @ <br /> TFC
+            </h2>
+          </div>
+        </Container>
       </section>
     </>
   );
+};
+
+const containerStyle = {
+  position: 'relative',
+  backgroundImage: `url(${backgroundImageUrl})`,
+  backgroundSize: 'cover', // Adjust as needed
+  backgroundPosition: 'center center', // Adjust as needed
+  backgroundRepeat: 'no-repeat', // Adjust as needed
+  width: '100%', // Set the width to 100% of the viewport width
+  height: '100vh', // Set the desired height
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'white', // Set text color
+  margin: 0, // Reset margin
+  padding: 0, // Reset padding
+  overflow: 'hidden',
+  boxSizing: 'border-box',
+  marginBottom: '40px',
+};
+
+const overlayStyle = {
+  position: 'absolute',
+  top: 0,
+  left: '200px',
+  width: '60%', // Adjust to position the overlay to the left
+  height: '100%',
+  backgroundColor: 'rgba(255, 255, 255, 0.8)', // Adjust overlay color and opacity
+  zIndex: 1,
+};
+
+const textStyle = {
+  position: 'relative', // Ensure text is above the overlay
+  zIndex: 2, // Higher z-index than overlay
+  textAlign: 'left',
+  marginLeft: '20px',
 };
 
 export default TFCsundays;
