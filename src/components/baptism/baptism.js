f4903684c2dcfccee5cecb4d81dc6948b5cee8fa
raji <kingraj1344@gmail.com>
2024-03-28 21:22:06 +0100

    Add Baptism component and import it in Baptism page

---
+++ src/components/baptism/baptism.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Style from './baptism.module.css';
+
+export const Baptismcontent = () => {
+    return (
+        <div>
+            <div className={Style.Hero}>
+                <div className={Style.Herotext}>
+                    <h1>Get Baptized</h1>
+                    <h4>an outward symbol of an inward decision</h4>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/pages/Baptism.js
+++ src/components/pages/Baptism.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header';
+import { Baptismcontent } from '../baptism/baptism';
 import Footer from '../Footer';
 
 function Baptism() {
   return (
     <div>
       <Header />
+      <Baptismcontent/>
       <Footer />
     </div>
   )
