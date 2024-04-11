034508ba92c13fb5bca7aa7e5fb95ddf3c4242ef
Folaranmi Israel <111568672+IsraelFIT@users.noreply.github.com>
2024-04-11 15:10:12 -0700

    Add files via upload

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Style from './Button.module.css'
 
-export const Button = ({style,children,href}) => {
+export const Button = ({style, children, href, className}) => {
   return (
-    <a href={href} className={Style.baptismButton} style={style}>{children}</a>
+    // <a href={href} className={Style.baptismButton} style={style}>{children}</a>
+    <a href={href} className={`${Style.baptismButton} ${className}`} style={style}>{children}</a>
   )
 }
