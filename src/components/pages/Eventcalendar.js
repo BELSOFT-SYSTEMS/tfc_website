95155c49b1dc52141d6d9ca82c2e5f2722366c57
cemek7 <106226729+cemek7@users.noreply.github.com>
2024-04-13 16:41:00 +0100

    Commit message for the addition of lightbox to joyforce

--- src/components/pages/Eventcalendar.js
+++ src/components/pages/Eventcalendar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
+import TFCeventscalendar from '../events-calendar/TFCevents-calendar';
+import TFCevent from '../events-calendar/TFCevent';
 
 function Eventcalendar() {
   return (
     <div>
       <Header />
+      <TFCevent />
+      <TFCeventscalendar />
       <Footer />
       
     </div>
