0a058891dfcd2df353e461bc6dd71e66d4f3ac97
raji <kingraj1344@gmail.com>
2024-04-13 14:42:34 +0100

    Update styling for HomeSlider and Mission components

--- src/components/about/Mission.js
+++ src/components/about/Mission.js
@@ -6,12 +6,12 @@ const Mission = () => {
     <>
       {/* Section 1 */}
       <section className="your-section-class">
-        <Container fluid style={{ display: 'flex', justifyContent: 'center' }}>
-          <div style={{ flex: 1, maxWidth: '500px', padding: '50px', textAlign: 'center' }}>
+        <Container fluid style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+          <div style={{ flex: '1 1 50%', maxWidth: '500px', padding: '50px', textAlign: 'center' }}>
             <h2 style={{ fontSize: '70px' }}>Mission.</h2>
             <p style={{ fontFamily: 'Montserrat,Sans-serif', fontSize: '20px', fontWeight: '400', lineHeight: '20px' }}>To glorify God by reaching and enabling<br /> people of all ages and nations to become<br /> fully devoted followers of Jesus Christ.</p>
           </div>
-          <div style={{ flex: 1, maxWidth: '500px', padding: '50px', textAlign: 'center' }}>
+          <div style={{ flex: '1 1 50%', maxWidth: '500px', padding: '50px', textAlign: 'center', marginTop: '0px' }}>
             <h2 style={{ fontSize: '70px' }}>Vision.</h2>
             <p style={{ fontFamily: 'Montserrat,Sans-serif', fontSize: '20px', fontWeight: '400', lineHeight: '20px' }}>To become a missionary church of 10,000<br /> meeting the physical, emotional, and <br />spiritual needs of people in Jesus’ name.</p>
           </div>
