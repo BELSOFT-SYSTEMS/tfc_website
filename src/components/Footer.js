6ab9c1f8d86ddbd528ec8313c5c34696714ce820
cemek7 <106226729+cemek7@users.noreply.github.com>
2024-04-15 23:57:26 +0100

    the addition of swiper to joyforce and our ministries pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleRight,  faPhone, faEnvelopeOpen, faMapPin, faEnvelope, faUser  } from '@fortawesome/free-solid-svg-icons';
+import {   faPhone,  faMapPin, faEnvelope,  } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelopeOpen as farEnvelopeOpen } from '@fortawesome/free-regular-svg-icons';
 import { faInstagram, faFacebook, faYoutube, } from '@fortawesome/free-brands-svg-icons';
 
@@ -9,157 +9,86 @@ import { faInstagram, faFacebook, faYoutube, } from '@fortawesome/free-brands-sv
 
 function Footer() {
   return (
-    <footer style={{ backgroundColor: '#012101', padding: '20px 0', color: 'white' }}>
-    <Container className="elementor-container elementor-column-gap-default">
-      {/* Container for the "Explore Your" and "Next Steps @ Sanctus" section */}
-{/* <Container>
-        
-              <div className="elementor-element elementor-element-ac707ff elementor-invisible elementor-widget elementor-widget-text-editor">
-                <div className="elementor-widget-container">
-                  <p>Explore Your</p>
-                </div>
-              </div>
-              <div className="elementor-element elementor-element-ab3ddf3 animated-slow elementor-invisible elementor-widget elementor-widget-heading">
-                <div className="elementor-widget-container">
-                  <h2 className="elementor-heading-title elementor-size-default">Next Steps @ TFC</h2>
-                </div>
-              </div>
+    <footer style={{ backgroundColor: '#012101', paddingTop:'10px', color: 'white' }}>
+    <Container>
+      {/* Use Bootstrap grid classes for responsive behavior */}
+      <Row>
+        {/* Get In Touch container */}
+        <Col xs={12} lg={6}>
+          <Container style={{ textAlign: 'left', paddingBottom:'25px' }}>
+            <div style={{ width: '100%' }}>
+              <h2 style={{ fontSize: '60px' }}>Get In Touch</h2>
             </div>
-        
-              <div className="elementor-element elementor-element-b082f66 elementor-hidden-mobile elementor-widget elementor-widget-spacer">
-                <div className="elementor-widget-container">
-                  <div className="elementor-spacer">
-                    <div className="elementor-spacer-inner"></div>
-                  </div>
-                </div>
-              </div>
-              <div className="elementor-element elementor-element-e1500c7 elementor-view-default elementor-widget elementor-widget-icon">
-                <div className="elementor-widget-container">
-                  <div className="elementor-icon-wrapper">
-                    <a className="elementor-icon elementor-animation-grow" href="https://sanctuschurch.com/next-steps/">
-                      <FontAwesomeIcon icon={faArrowAltCircleRight} style={{ color: '#d49c04',  }} />
-                    </a>
-                  </div>
-                </div>
-              </div>
+            <div style={{ listStyleType: 'none', paddingLeft: '0' }}>
+              <Row>
+                <Col>
+                  <FontAwesomeIcon icon={faPhone} className="elementor-icon-list-icon" style={{ color: '#d49c04' }} />
+                  <span style={{ paddingLeft: '10px' }} >(905) 686-4450</span>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <FontAwesomeIcon icon={farEnvelopeOpen} className="elementor-icon-list-icon" style={{ color: '#d49c04' }} />
+                  <span style={{ paddingLeft: '10px' }}>hello@TheFathersChurch.com</span>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <FontAwesomeIcon icon={faMapPin} className="elementor-icon-list-icon" style={{ color: '#d49c04' }} />
+                  <span style={{ paddingLeft: '10px' }}>Main Office 599 Bayly St East Ajax, ON L1Z 1L4</span>
+                </Col>
+              </Row>
             </div>
-          </Col>
-        </Row>
-      </Container> */}
-
-
-      <Container style={{ display: 'flex' , justifyContent: 'space-between' }}>
-      {/* Container for the "Get In Touch" heading and contact information */}
-      <Container style={{ textAlign: 'left' }}>
-        <Row>
-          <Col md={6}>
-
-            <div className="elementor-element elementor-element-0e0ec13 elementor-widget elementor-widget-heading">
-
-            
-              <div className="elementor-widget-container" style={{width:'190px' }}>
-                <h2 className="elementor-heading-title elementor-size-default" style={{fontSize:'45px'}}>Get In Touch</h2>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <Row style={{ listStyleType: 'none' }}>
-        <Col md={12}>
-      {/* Apply custom styles for the list */}
-      <div style={{ listStyleType: 'none', paddingLeft: '0' }}>
-          <Col >
-            <FontAwesomeIcon icon={faPhone} className="elementor-icon-list-icon" style={{ color: '#d49c04',  }}/>
-            <span style={{paddingLeft:'10px'}} >(905) 686-4450</span>
-          </Col>
-          <Col >
-            <FontAwesomeIcon icon={farEnvelopeOpen} className="elementor-icon-list-icon" style={{ color: '#d49c04',  }} />
-            <span style={{paddingLeft:'10px'}}>hello@TheFathersChurch.com</span>
-          </Col>
-          <Col >
-            <FontAwesomeIcon icon={faMapPin} className="elementor-icon-list-icon" style={{ color: '#d49c04',  }} />
-            <span style={{paddingLeft:'10px'}}>Main Office 599 Bayly St East Ajax, ON L1Z 1L4</span>
-          </Col>
-          </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <div style={{paddingTop:'10px'}} >
-              <span  style={{ marginLeft: '10px',}}>
+            <div style={{ paddingTop: '10px', paddingBottom:'20px' }} >
+              <span style={{ marginLeft: '10px' }}>
                 <a href='https://www.facebook.com/' target="_blank" rel="noopener noreferrer">
-                  {/*<span className="elementor-screen-only">Facebook</span>*/}
-                  <FontAwesomeIcon icon={faFacebook} size='3x' style={{ color: '#d49c04',  }} />
+                  <FontAwesomeIcon icon={faFacebook} size='3x' style={{ color: '#d49c04' }} />
                 </a>
               </span>
-              <span className="elementor-grid-item" style={{ marginLeft: '10px', }}>
+              <span className="elementor-grid-item" style={{ marginLeft: '10px' }}>
                 <a href='https://www.instagram.com/' target="_blank" rel="noopener noreferrer">
-                  {/*<span className="elementor-screen-only">Instagram</span>*/}
-                  <FontAwesomeIcon icon={faInstagram} size='3x' style={{ color: '#d49c04',  }} />
+                  <FontAwesomeIcon icon={faInstagram} size='3x' style={{ color: '#d49c04' }} />
                 </a>
               </span>
-              <span className="elementor-grid-item" style={{ marginLeft: '10px', }}>
-                <a  href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-                  {/*<span className="elementor-screen-only">Youtube</span>*/}
-                  <FontAwesomeIcon icon={faYoutube} size='3x'style={{ color: '#d49c04',  }} />
+              <span className="elementor-grid-item" style={{ marginLeft: '10px' }}>
+                <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faYoutube} size='3x' style={{ color: '#d49c04' }} />
                 </a>
               </span>
             </div>
-          </Col>
-        </Row>
-      </Container>
-  
-      {/* Container for the social media icons */}
-      
-      
-  
-      {/* Container for the "Subscribe to receive updates" section */}
-      <Container style={{ textAlign: 'left' }}>
-        <Row>
-          <Col>
-            <h2 style={{fontSize:'25px'}}>Subscribe to receive updates from <br/>The Father's Church</h2>
-          </Col>
-        </Row>
-        <Row className="justify-content-start">
-          <Col md={9} className="d-flex justify-content-start">
-    <Container style={{ paddingRight: '15px', textAlign: 'left' }}>
-      <Form className="elementor-form" method="post" id="newsletter_subscribe_form" name="Newsletter Subscribe" style={{ textAlign: 'left' }}>
-        {/* ... (your form code) ... */}
-        <input type="hidden" name="post_id" value="9361" />
-        <input type="hidden" name="form_id" value="c886317" />
-        <input type="hidden" name="referer_title" value="Home - Sanctus Church" />
-        <input type="hidden" name="queried_id" value="19128" />
-
-        <Form.Group controlId="formEmail" style={{ marginTop: '10px' }}>
-          <Form.Control type="email" name="form_fields[name]" placeholder="Email" style={{ maxWidth: '350px' }} />
-        </Form.Group>
-
-        <Form.Group controlId="formName" style={{ marginTop: '10px' }}>
-          <Form.Control type="text" name="form_fields[field_a7d8490]" placeholder="Name" style={{ maxWidth: '350px' }} />
-        </Form.Group>
-
-        <div className="e-form__buttons" style={{ marginTop: '10px' }}>
-          <Button type="submit" variant='warning' className="elementor-button elementor-size-sm elementor-animation-grow" style={{backgroundColor: '#d49c04', color: '#000',}}>
-            <span>
-              <span className="elementor-button-icon">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </span>
-              <span className="elementor-button-text" style={{paddingLeft:'10px'}}>Send</span>
-            </span>
-          </Button>
-        </div>
-      </Form>
-    </Container>
-  </Col>
-</Row>
-        <Row>
-        <Col md={12}>
-          <p style={{ color: 'white' }}>© 2024 All rights Reserved. Design by Creative Media</p>
+          </Container>
+        </Col>
+        {/* Subscribe form container */}
+        <Col xs={12} lg={6}>
+          <Container style={{ paddingRight: '15px', textAlign: 'left' }}>
+            <h2 style={{ fontSize: '25px' }}>Subscribe to receive updates from <br />The Father's Church</h2>
+            <Form method="post" id="newsletter_subscribe_form" name="Newsletter Subscribe" style={{ textAlign: 'left' }}>
+              {/* ... (your form code) ... */}
+              <Form.Group controlId="formEmail" style={{ marginTop: '10px' }}>
+                <Form.Control type="email" name="form_fields[name]" placeholder="Email" style={{ maxWidth: '350px' }} />
+              </Form.Group>
+              <Form.Group controlId="formName" style={{ marginTop: '10px' }}>
+                <Form.Control type="text" name="form_fields[field_a7d8490]" placeholder="Name" style={{ maxWidth: '350px' }} />
+              </Form.Group>
+              <div style={{ marginTop: '10px' }}>
+                <Button type="submit" variant='warning' style={{ backgroundColor: '#d49c04', color: '#000' }}>
+                  <span>
+                    <span >
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </span>
+                    <span  style={{ paddingLeft: '10px' }}>Send</span>
+                  </span>
+                </Button>
+              </div>
+            </Form>
+          </Container>
         </Col>
       </Row>
-      </Container>
-      </Container>
-  
-      
+      <Row>
+        <Col>
+          <p style={{ color: 'white' }}>© 2024 All rights Reserved. Design by Our Creative Media</p>
+        </Col>
+      </Row>
     </Container>
   </footer>
   )
--- src/components/governors/Ourvalues.js
+++ src/components/governors/Ourvalues.js
@@ -1,66 +1,84 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState, useRef, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import './ImageScroll.css';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
+function ImageScroll() {
+  const images = [
+    '/joy/joy1.jpeg',
+    '/joy/joy2.jpeg',
+    '/joy/joy3.jpg',
+    '/joy/joy4.jpg',
+    '/joy/joy5.jpg',
+    '/joy/joy6.jpg',
+  ];
 
-const Ourvalues = () => {
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  const swiperRef = useRef(null);
 
-  
+  const toggleOpen = (state) => () => setOpen(state);
+
+  const handleImageClick = (index) => {
+    setIndex(index);
+    toggleOpen(true)();
+  };
+
+  useEffect(() => {
+    const swiper = swiperRef.current.swiper;
+    const intervalId = setInterval(() => {
+      if (swiper) {
+        swiper.slideNext();
+      }
+    }, 5000); // Slide every 5 seconds
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
-    <>
-
-      {/* Section 1 */}
-      <section className="your-section-class">
-      <Container fluid >
-        <h1 style={{fontFamily:'Montserrat,Sans-serif', fontSize:'80px', textAlign:'center', fontWeight:'bolder'}}>Our Values</h1>
-        <div style={{marginLeft:'60px', paddingLeft:'60px'}}>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', marginRight: '20px', marginTop:'50px', marginBottom:'50px',alignItems:'center', justifyContent: 'center' }}>
-        <img src="icons/bible.webp" alt="Icon" style={{ width: '98px', height: '98px', marginRight: '20px' }} />
-          <div>
-          <h3 style={{fontFamily:'Sans-serif', fontSize:'25px', fontWeight:'500', lineHeight:'30px'}}>
-          CREATIVE BIBLICAL TEACHING OF GOD'S WORD.</h3>
-          </div>
-          </div>
-          <div style={{ display: 'flex', alignItems:'center', marginLeft: '20px', marginTop:'50px', marginBottom:'50px' }}>
-          <img src="icons/people.webp" alt="Icon" style={{ width: '98px', height: '98px', marginRight: '20px' }} />
-            <div>
-            <h3 style={{fontFamily:'Sans-serif', fontSize:'25px', fontWeight:'500', lineHeight:'30px'}}>
-            RELATIONSHIPS, FRIENDSHIPS, AND MENTORING.</h3>
-            </div>
-            </div>
-            <div style={{ display: 'flex', alignItems:'center', marginRight: '20px', marginTop:'50px', marginBottom:'50px' }}>
-            <img src="icons/celebration.jpeg" alt="Icon" style={{ width: '98px', height: '98px', marginRight: '20px' }} />
-          <div>
-          <h3 style={{fontFamily:'Sans-serif', fontSize:'25px', fontWeight:'500', lineHeight:'30px'}}>
-          CELEBRATING JESUS AND HAVING FUN TOGETHER</h3>
-          </div>
-          </div>
-            </div>
-            <div style={{ display: 'flex', alignItems:'center', justifyContent: 'space-between' }}>
-            <div style={{ display: 'flex', marginRight: '20px', alignItems:'center', justifyContent: 'center', marginTop:'50px', marginBottom:'50px' }}>
-            <img src="icons/welcome.jpeg" alt="Icon" style={{ width: '98px', height: '98px', marginRight: '20px' }} />
-          <div>
-          <h3 style={{fontFamily:'Sans-serif', fontSize:'25px', fontWeight:'500', lineHeight:'30px'}}>
-          WELCOMING EVERYONE, SAFETY, AND BELONGING.</h3>
-          </div>
-          </div>
-          <div style={{ display: 'flex', marginRight: '20px',alignItems:'center', justifyContent: 'center', marginTop:'50px', marginBottom:'50px' }}>
-          <img src="icons/tech.jpeg" alt="Icon" style={{ width: '98px', height: '98px', marginRight: '20px' }} />
-          <div>
-          <h3 style={{fontFamily:'Sans-serif', fontSize:'25px', fontWeight:'500', lineHeight:'30px'}}>
-          LEVERAGING TECHNOLOGY FOR EFFECTIVE MINISTRY.</h3>
-          </div>
-          </div>
-
-            </div>
-            </div>
-            
-</Container>
-      </section>
-    </>
+    <div>
+      <Swiper
+        ref={swiperRef}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        loop={true} // Enable loop mode
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt="slide_image" onClick={() => handleImageClick(index)} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/*<Lightbox
+        open={open}
+        onClose={toggleOpen(false)}
+        index={index}
+        slides={images}
+        animation={{ fade: 0 }}
+        controller={{ closeOnPullDown: true, closeOnBackdropClick: true }}
+      />*/}
+    </div>
   );
-};
+}
 
-export default Ourvalues;
+export default ImageScroll;
