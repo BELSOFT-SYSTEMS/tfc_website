8003cf1c7fa123bab6a11a05864a1db97660f721
Olaleye Igbagbo <89357459+igee-17@users.noreply.github.com>
2024-05-04 21:41:42 +0100

    location tap updates

--- src/components/LocationTab.js
+++ src/components/LocationTab.js
@@ -5,9 +5,9 @@ import { faCalendarAlt, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons
 
 const LocationTab = ({ eventKey }) => {
     return (
-        <Container fluid style={{ border: '2px solid red', marginTop: '30px' }}>
-            <Row style={{ display: 'flex', gap: '30px' }}>
-                <Col xs={12} md={6} className="text-center" style={{ width: 'fit-content', display: 'flex', flexDirection: 'column' }}>
+        <Container fluid style={{ marginTop: '30px' }}>
+            <div style={{ display: 'flex', gap: '50px' }}>
+                <div className="text-center" style={{ width: 'fit-content', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                     <img
                         src="pastor-3.jpg"
                         alt=""
@@ -16,40 +16,40 @@ const LocationTab = ({ eventKey }) => {
                     />
                     <h2 style={{ fontSize: '25px', textTransform: 'uppercase', letterSpacing: '-0.9px', marginTop: '20px', marginBottom: '5px' }}>Angela Mason</h2>
                     <p style={{ fontSize: '18px', textAlign: 'center', textTransform: 'uppercase', fontWeight: '500' }}>Site Pastor</p>
-                </Col>
-                <Col xs={0} md={6}>
-                    <Row>
-                        <Col>
-                            <ListGroup variant="flush">
+                </div>
+                <div xs={0} md={6} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+                    <div >
+                        <div>
+                            <FontAwesomeIcon icon={faCalendarAlt} style={{ color: '#d49c04', width: '15px' }} className="mr-2" />
+                            <span style={{ color: '#d49c04', width: '15px', marginLeft: '10px', fontSize: '18px', fontWeight: '500', textTransform: 'uppercase' }}>Sunday services @ 9:00am & 11:00am</span>
+                        </div>
+                        <div style={{ marginTop: '10px' }}>
+                            <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: '#d49c04', width: '15px' }} className="mr-2" />
+                            <span style={{ color: '#d49c04', width: '15px', marginLeft: '10px', fontSize: '18px', fontWeight: '500', textTransform: 'uppercase' }}>Sunday services @ 9:00am & 11:00am</span>
+                        </div>
+                        {/* <ListGroup variant="flush">
                                 <ListGroup.Item style={{ fontSize: '20px', display: 'flex', gap: '15px', alignItems: 'center' }}>
-                                    <FontAwesomeIcon icon={faCalendarAlt} style={{ color: '#d49c04', width: '15px' }} className="mr-2" />
-                                    <span>Sunday services @ 9:00am & 11:00am</span>
                                 </ListGroup.Item>
                                 <ListGroup.Item style={{ fontSize: '20px', display: 'flex', gap: '15px', alignItems: 'center' }}>
-                                    <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: '#d49c04', width: '15px' }} className="mr-2" />
-                                    <span>599 Bayly Street East, Ajax, Ontario</span>
                                 </ListGroup.Item>
-                            </ListGroup>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col style={{ fontSize: '15px' }}>
-                            <p style={{ fontWeight: '300', fontSize: '18px' }}>Filled with warm and inviting people, Ajax acts as our largest in-person site with live teaching, worship and many events that help foster and build up our community.</p>
-                        </Col>
-                    </Row>
+                            </ListGroup> */}
+                    </div>
+                    <div >
+                        <p style={{ fontWeight: '300', fontSize: '18px' }}>Filled with warm and inviting people, Ajax acts as our largest in-person site with live teaching, worship and many events that help foster and build up our community.</p>
+                    </div>
                     <div>
-                        <div className="text-center" style={{ width: '100%', displa: 'flex', justifyContent: 'flex-start', border: '2px solid red', alignItems: 'flex-start' }}>
+                        <div className="text-center" style={{ width: '100%', displa: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start', textAlign: 'start' }}>
                             <button
                                 variant=""
-                                style={{ backgroundColor: '#d49c04', color: '#fff', maxWidth: '200px', marginLeft: '0' }}
+                                style={{ backgroundColor: '#d49c04', color: '#fff', maxWidth: '200px', marginLeft: '0', border: 'none', padding: '8px 16px', borderRadius: '5px', textTransform: 'uppercase', fontWeight: '500' }}
                                 href="#"
                             >
                                 More Information
                             </button>
                         </div>
                     </div>
-                </Col>
-            </Row>
+                </div>
+            </div>
         </Container>
     )
 }
