0797e5a1ea0eb0099358fa7442b968ac17ad8668
Folaranmi Israel <111568672+IsraelFIT@users.noreply.github.com>
2024-05-08 08:33:00 -0700

    updates to locations

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { Tab, Tabs, Container, Row, Col, ButtonGroup, Button, ListGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChurch, faCalendarAlt, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
-import LocationTab from './LocationTab';
 
 const Locations = () => {
   const [activeKey, setActiveKey] = useState('eden');
@@ -15,35 +14,211 @@ const Locations = () => {
   };
 
   return (
-    <div style={{ padding: '20px 96px', margin: '0 10rem' }}>
+    <div style={{ paddingTop: '20px', paddingBottom: '40px' }}>
       <Container style={{ justifyContent: 'center', textAlign: 'center', marginTop: '30px', marginBottom: '20px' }}>
         <h2 style={{ fontSize: '55px' }}> Our Locations</h2>
-        <p style={{ fontSize: '20px', justifyContent: 'center', textAlign: 'center' }}>Join us at our sites across Abuja</p>
+        <p style={{ fontSize: '18px', textAlign: 'center', textTransform: 'uppercase' }}>Join us at our sites across Abuja</p>
       </Container>
 
-      <ButtonGroup size="lg" style={{
-        justifyContent: 'center',
-        display: 'flex',
-        flexDirection: flexDirection,
-        textAlign: 'center',
-        height: '40px',
-        marginBottom: '20px',
-        gap: '15px'
-      }}>
-        <Button variant="outline-success" onClick={() => handleSelect('eden')} active={activeKey === 'eden'}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04' }} />Eden</Button>
-        <Button variant="outline-success" onClick={() => handleSelect('mpape')} active={activeKey === 'mpape'}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04' }} />Mpape</Button>
-        <Button variant="outline-success" onClick={() => handleSelect('enugu')} active={activeKey === 'enugu'}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04' }} />Enugu</Button>
-        <Button variant="outline-success" onClick={() => handleSelect('online')} active={activeKey === 'online'}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04' }} />Online</Button>
+      <ButtonGroup size="lg" style={{ justifyContent: 'center', display: 'flex', flexDirection: flexDirection, textAlign: 'center', margin: '0 60px', gap: '15px', paddingBottom: '60px', height: '100px' }}>
+        <Button variant="outline-success" onClick={() => handleSelect('eden')} active={activeKey === 'eden'} style={{ fontSize: '16px', fontWeight: '600' }}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04', }} /> Eden</Button>
+        <Button variant="outline-success" onClick={() => handleSelect('mpape')} active={activeKey === 'mpape'} style={{ fontSize: '16px', fontWeight: '600' }}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04' }} /> Mpape</Button>
+        <Button variant="outline-success" onClick={() => handleSelect('enugu')} active={activeKey === 'enugu'} style={{ fontSize: '16px', fontWeight: '600' }}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04' }} /> Enugu</Button>
+        <Button variant="outline-success" onClick={() => handleSelect('online')} active={activeKey === 'online'} style={{ fontSize: '16px', fontWeight: '600' }}><FontAwesomeIcon icon={faChurch} style={{ color: '#d49c04' }} /> Online</Button>
       </ButtonGroup>
 
       <Tabs id="locations-tabs" activeKey={activeKey} onSelect={handleSelect} className="eael-tabs-nav">
-        <Tab eventKey='eden'>
-          <LocationTab />
+        <Tab eventKey="eden">
+          <Container fluid>
+            <Row>
+              <Col xs={12} md={5} className="text-center" style={{ marginTop: '40px' }}>
+                <img
+                  src="pastor-3.jpg"
+                  alt=""
+                  className="img-fluid rounded-circle"
+                  style={{ maxWidth: '300px', height: '300px', objectFit: 'cover' }}
+                />
+                <h2 style={{ fontSize: '32px', fontWeight: '600' }}>Pastor Ikenna Okeke</h2>
+                <h2 style={{ fontSize: '16px', fontWeight: '500' }}>Senior Pastor</h2>
+              </Col>
+              <Col xs={12} md={7} style={{ padding: '0 60px', }}>
+                <div>
+                  <div style={{ display: 'flex', flexDirection: 'column', marginTop: '60px', padding: '0' }}>
+                    <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                      <FontAwesomeIcon icon={faCalendarAlt} style={{ color: '#d49c04' }} className="mr-2" />      Sunday services @ 9:00am & 11:00am
+                    </span>
+                    <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                      <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: '#d49c04' }} className="mr-2" />      599 Bayly Street East, Ajax, Ontario
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <p style={{ fontSize: '16px', fontWeight: '500' }}>Filled with warm and inviting people, 
+                  Ajax acts as our largest in-person site with live teaching, 
+                  worship and many events that help foster and build up our community.</p>
+                </div>
+                <div>
+                  <Button
+                    variant="success"
+                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#d49c04', color: '#fff', width:'200px', height: '40px', textTransform: 'uppercase', fontSize: '16px', fontWeight: '600', border: 'none', }}
+                    href="#"
+                  > 
+                    More Information
+                  </Button>
+                </div>
+                {/* <Row>
+                  <Col className="text-center">
+                    
+                  </Col>
+                </Row> */}
+              </Col>
+            </Row>
+          </Container>
         </Tab>
-        <Tab eventKey='mpape'>
-          <LocationTab />
+        
+        <Tab eventKey="mpape">
+          <Container fluid>
+              <Row>
+                <Col xs={12} md={5} className="text-center" style={{ marginTop: '40px' }}>
+                  <img
+                    src="pastor-3.jpg"
+                    alt=""
+                    className="img-fluid rounded-circle"
+                    style={{ maxWidth: '300px', height: '300px', objectFit: 'cover' }}
+                  />
+                  <h2 style={{ fontSize: '32px', fontWeight: '600' }}>Pastor Ikenna Okeke</h2>
+                  <h2 style={{ fontSize: '16px', fontWeight: '500' }}>Senior Pastor</h2>
+                </Col>
+                <Col xs={12} md={7} style={{ padding: '0 60px', }}>
+                  <div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '60px', padding: '0' }}>
+                      <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                        <FontAwesomeIcon icon={faCalendarAlt} style={{ color: '#d49c04' }} className="mr-2" />      Sunday services @ 9:00am & 11:00am
+                      </span>
+                      <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: '#d49c04' }} className="mr-2" />      599 Bayly Street East, Ajax, Ontario
+                      </span>
+                    </div>
+                  </div>
+                  <div>
+                    <p style={{ fontSize: '16px', fontWeight: '500' }}>Filled with warm and inviting people, 
+                    Ajax acts as our largest in-person site with live teaching, 
+                    worship and many events that help foster and build up our community.</p>
+                  </div>
+                  <div>
+                    <Button
+                      variant="success"
+                      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#d49c04', color: '#fff', width:'200px', height: '40px', textTransform: 'uppercase', fontSize: '16px', fontWeight: '600', border: 'none', }}
+                      href="#"
+                    > 
+                      More Information
+                    </Button>
+                  </div>
+                  {/* <Row>
+                    <Col className="text-center">
+                      
+                    </Col>
+                  </Row> */}
+                </Col>
+              </Row>
+          </Container>
         </Tab>
-        {/* Add more tabs here with similar structure */}
+
+        <Tab eventKey="enugu">
+          <Container fluid>
+              <Row>
+                <Col xs={12} md={5} className="text-center" style={{ marginTop: '40px' }}>
+                  <img
+                    src="pastor-3.jpg"
+                    alt=""
+                    className="img-fluid rounded-circle"
+                    style={{ maxWidth: '300px', height: '300px', objectFit: 'cover' }}
+                  />
+                  <h2 style={{ fontSize: '32px', fontWeight: '600' }}>Pastor Ikenna Okeke</h2>
+                  <h2 style={{ fontSize: '16px', fontWeight: '500' }}>Senior Pastor</h2>
+                </Col>
+                <Col xs={12} md={7} style={{ padding: '0 60px', }}>
+                  <div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '60px', padding: '0' }}>
+                      <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                        <FontAwesomeIcon icon={faCalendarAlt} style={{ color: '#d49c04' }} className="mr-2" />      Sunday services @ 9:00am & 11:00am
+                      </span>
+                      <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: '#d49c04' }} className="mr-2" />      599 Bayly Street East, Ajax, Ontario
+                      </span>
+                    </div>
+                  </div>
+                  <div>
+                    <p style={{ fontSize: '16px', fontWeight: '500' }}>Filled with warm and inviting people, 
+                    Ajax acts as our largest in-person site with live teaching, 
+                    worship and many events that help foster and build up our community.</p>
+                  </div>
+                  <div>
+                    <Button
+                      variant="success"
+                      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#d49c04', color: '#fff', width:'200px', height: '40px', textTransform: 'uppercase', fontSize: '16px', fontWeight: '600', border: 'none', }}
+                      href="#"
+                    > 
+                      More Information
+                    </Button>
+                  </div>
+                  {/* <Row>
+                    <Col className="text-center">
+                      
+                    </Col>
+                  </Row> */}
+                </Col>
+              </Row>
+          </Container>
+        </Tab>
+
+        <Tab eventKey="online">
+          <Container fluid>
+              <Row>
+                <Col xs={12} md={5} className="text-center" style={{ marginTop: '40px' }}>
+                  <img
+                    src="pastor-3.jpg"
+                    alt=""
+                    className="img-fluid rounded-circle"
+                    style={{ maxWidth: '300px', height: '300px', objectFit: 'cover' }}
+                  />
+                  <h2 style={{ fontSize: '32px', fontWeight: '600' }}>Pastor Ikenna Okeke</h2>
+                  <h2 style={{ fontSize: '16px', fontWeight: '500' }}>Senior Pastor</h2>
+                </Col>
+                <Col xs={12} md={7} style={{ padding: '0 60px', }}>
+                  <div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '60px', padding: '0' }}>
+                      <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                        <FontAwesomeIcon icon={faCalendarAlt} style={{ color: '#d49c04' }} className="mr-2" />      Sunday services @ 9:00am & 11:00am
+                      </span>
+                      <span style={{ fontSize: '16px', fontWeight: '600', textTransform: 'uppercase' }}>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: '#d49c04' }} className="mr-2" />      599 Bayly Street East, Ajax, Ontario
+                      </span>
+                    </div>
+                  </div>
+                  <div>
+                    <p style={{ fontSize: '16px', fontWeight: '500' }}>Filled with warm and inviting people, 
+                    Ajax acts as our largest in-person site with live teaching, 
+                    worship and many events that help foster and build up our community.</p>
+                  </div>
+                  <div>
+                    <Button
+                      variant="success"
+                      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#d49c04', color: '#fff', width:'200px', height: '40px', textTransform: 'uppercase', fontSize: '16px', fontWeight: '600', border: 'none', }}
+                      href="#"
+                    > 
+                      More Information
+                    </Button>
+                  </div>
+                  {/* <Row>
+                    <Col className="text-center">
+                      
+                    </Col>
+                  </Row> */}
+                </Col>
+              </Row>
+          </Container>
+        </Tab>
       </Tabs>
     </div>
   );
