3e838aaecb209c72dd8025fff0dea0e2c2174021
BELSOFT SYSTEMS <149635505+BELSOFT-SYSTEMS@users.noreply.github.com>
2024-05-08 15:09:12 +0100

    images removed cdn implemented

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,176 +1,177 @@
 export const data = [
     {
-      url: '/sundays/sundays-1.jpg',
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715175061/TFC/sundays/sundays-1_fgkxlt.jpg',
       description: 'Description for image 1'
     },
     {
-      url: '/sundays/sundays-2.jpg',
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176185/TFC/sundays/sundays-41_v2xboy.jpg',
       description: 'Description for image 2'
     },
     {
-      url: '/sundays/sundays-31.jpg',
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176134/TFC/sundays/sundays-30_dlruix.jpg',
       description: 'Description for image 3'
     },
     {
-      url: '/sundays/sundays-34.jpg',
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176149/TFC/sundays/sundays-33_b0gujw.jpg',
       description: 'Description for image 4'
     },
     {
-      url: '/sundays/sundays-10.jpg',
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176214/TFC/sundays/sundays-13_qad2je.jpg',
       description: 'Description for image 5'
     },
     {
-      url: '/sundays/sundays-43.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176269/TFC/sundays/sundays-19_diym54.jpg',
+      description: 'Description for image 6'
     },
     {
-      url: '/sundays/sundays-40.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-34_cttwd2.jpg',
+      description: 'Description for image 7'
     },
     {
-      url: '/sundays/sundays-42.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-40_cttwd2.jpg',
+      description: 'Description for image 8'
     },
     {
-      url: '/sundays/sundays-41.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-2_cttwd2.jpg',
+      description: 'Description for image 9'
     },
     {
-      url: '/sundays/sundays-35.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-3_cttwd2.jpg',
+      description: 'Description for image 10'
     },
     {
-      url: '/sundays/sundays-36.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-4_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-37.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-5_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-38.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-6_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-39.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-7_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-3.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-8_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-4.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-9_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-5.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-10_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-6.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-11_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-7.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-12_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-8.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-13_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-9.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-14_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-11.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-15_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-12.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-16_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-13.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-17_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-15.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-18_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-16.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-43_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-17.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-20_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-18.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-21_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-20.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-22_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-21.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-23_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-22.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-24_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-23.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-25_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-24.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-26_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-25.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-27_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-26.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-28_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-27.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-29_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-28.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-37_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-29.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-31_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-30.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-32_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-32.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-36_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-33.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-38_cttwd2.jpg',
+      description: 'Description for image 11'
     },
     {
-      url: '/sundays/sundays-13.jpg',
-      description: 'Description for image 5'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-39_cttwd2.jpg',
+      description: 'Description for image 11'
     },
+    
   
     {
-      url: '/sundays/sundays-14.jpg',
-      description: 'Description for image 6'
+      url: 'https://res.cloudinary.com/dbnrmhckx/image/upload/v1715176271/TFC/sundays/sundays-42_cttwd2.jpg',
+      description: 'Description for image 12'
     }
   ];
 
